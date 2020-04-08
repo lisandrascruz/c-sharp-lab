ec750f1ec447b329206bdf80e1a7e880c243f607
lisansouza <lisansouza@gmail.com>
2020-04-08 14:39:30 -0300

    draw-strings

---
+++ 00-drawing/Drawing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("The gost");
+
+            Console.WriteLine("c_    ____");
+            Console.WriteLine("  \\\\__|__|__");
+            Console.WriteLine("    \\( o_o)");
+            Console.WriteLine("　　　 > ~  >");
+            Console.WriteLine("　　　/ 　 / \\\\");
+            Console.WriteLine("　　 /　　/　 \\\\");
+            Console.WriteLine("　　 \\　 )　　c/");
+            Console.WriteLine("　　/　 /");
+            Console.WriteLine("　 /　/|");
+            Console.WriteLine("　(　( \\");
+            Console.WriteLine("　|　|  \\");
+            Console.WriteLine("　| / \\  )");
+            Console.WriteLine("　| |　) |");
+            Console.WriteLine(" /  )(_/");
+            Console.WriteLine("(_ /");
+            Console.WriteLine("Dancing Man");
+        }
+    }
+}
